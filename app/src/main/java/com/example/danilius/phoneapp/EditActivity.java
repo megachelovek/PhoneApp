27994e19b06162c90fa8a0b3a2b77c0abad47329
Danila Malchikov <2regvk@mail.ru>
2018-10-06 21:53:29 +0400

    Первый коммит

---
+++ app/src/main/java/com/example/danilius/phoneapp/EditActivity.java
@@ -0,0 +1,31 @@
+package com.example.danilius.phoneapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+    public TextView namefield, phonefield, emailfield;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(20);
+        textView.setPadding(16, 16, 16, 16);
+        namefield = (TextView) findViewById(R.id.name);
+        phonefield = (TextView) findViewById(R.id.phone_number);
+        emailfield = (TextView) findViewById(R.id.email);
+        Bundle arguments = getIntent().getExtras();
+        final PhoneBook phoneBook;
+        if(arguments!=null){
+            phoneBook = (PhoneBook) arguments.getSerializable(PhoneBook.class.getSimpleName());
+            namefield.setText(phoneBook.getName());
+            phonefield.setText(phoneBook.getName());
+            emailfield.setText(phoneBook.getName());
+
+        }
+        setContentView(textView);
+    }
+}
--- app/src/main/java/com/example/danilius/phoneapp/MainActivity.java
+++ app/src/main/java/com/example/danilius/phoneapp/MainActivity.java
@@ -1,32 +1,98 @@
 package com.example.danilius.phoneapp;
 
-import android.content.pm.PackageManager;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView lvPhone;
+    private TextView selection;
+    private List<PhoneBook> listPhoneBook = new ArrayList<PhoneBook>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PackageManager pm = getPackageManager();
-        boolean isTelephonySupported = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
-        boolean isGSMSupported = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY_GSM);
-        TelephonyManager.listen(stateListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+
+        // получаем элемент TextView
+        selection = (TextView) findViewById(R.id.selection);
+        // получаем элемент ListView
+        lvPhone = (ListView) findViewById(R.id.listPhone);
+
+
+        listPhoneBook.add(new PhoneBook("Pete Houston", "010-9817-6331", "pete.houston.17187@gmail.com"));
+        listPhoneBook.add(new PhoneBook("Lina Cheng", "046-7764-1142", "lina.cheng011@sunny.com"));
+        listPhoneBook.add(new PhoneBook("Jenny Nguyen", "0913-223-498", "jenny_in_love98@yahoo.com"));
+
+        // создаем адаптер
+        PhoneBookAdapter adapter = new PhoneBookAdapter(this, listPhoneBook);
+        // устанавливаем для списка адаптер
+        lvPhone.setAdapter(adapter);
+        // добвляем для списка слушатель
+        lvPhone.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                // по позиции получаем выбранный элемент
+                PhoneBook selectedItem = (PhoneBook)listPhoneBook.get(position);
+                // установка текста элемента TextView
+                Intent intent = new Intent(com.example.danilius.phoneapp.MainActivity.this,EditActivity.class);
+                intent.putExtra(PhoneBook.class.getSimpleName(),selectedItem);
+                startActivity(intent);
+            }
+        });
     }
-    PhoneStateListener stateListener = new PhoneStateListener() {
-        public void onCallStateChanged(int state, String incomingNumber) {
-            switch (state) {
-                case TelephonyManager.CALL_STATE_IDLE: break;
-                case TelephonyManager.CALL_STATE_OFFHOOK: break;
-                case TelephonyManager.CALL_STATE_RINGING:
-                    doMagicWork(incomingNumber); // Поступил звонок с номера incomingNumber
-                    break;
+}
+/*
+public class MainActivity extends Activity {
+
+    private ListView lvPhone;
+    private TextView selection;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        selection = (TextView) findViewById(R.id.selection);
+        lvPhone = (ListView)findViewById(R.id.listPhone);
+
+        List<PhoneBook> listPhoneBook = new ArrayList<PhoneBook>();
+        listPhoneBook.add(new PhoneBook("Pete Houston", "010-9817-6331", "pete.houston.17187@gmail.com"));
+        listPhoneBook.add(new PhoneBook(
+
+                "Lina Cheng", "046-7764-1142", "lina.cheng011@sunny.com"));
+        listPhoneBook.add(new PhoneBook(
+
+                "Jenny Nguyen", "0913-223-498", "jenny_in_love98@yahoo.com"));
+        PhoneBookAdapter adapter = new PhoneBookAdapter(this, listPhoneBook);
+        lvPhone.setAdapter(adapter);
+
+        // добвляем для списка слушатель
+        listPhoneBook.setOnItemClickListener{
+
+
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // по позиции получаем выбранный элемент
+                String selectedItem = lvPhone[position];
+                // установка текста элемента TextView
+                selection.setText(selectedItem);
             }
         }
-    };
-
-}
+}*/
